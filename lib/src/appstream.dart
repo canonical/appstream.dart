dbe39f68622ff3f2f1ff53b023a91cf8a9280731
Robert Ancell <robert.ancell@canonical.com>
2021-05-31 16:41:27 +1200

    Load icons

--- lib/src/appstream.dart
+++ lib/src/appstream.dart
@@ -1,29 +1,76 @@
 import 'package:xml/xml.dart';
 import 'package:yaml/yaml.dart';
 
+class AppstreamIcon {
+  const AppstreamIcon();
+}
+
+class AppstreamStockIcon extends AppstreamIcon {
+  final String name;
+
+  const AppstreamStockIcon(this.name);
+
+  @override
+  String toString() => '$runtimeType($name)';
+}
+
+class AppstreamCachedIcon extends AppstreamIcon {
+  final String name;
+  final int? width;
+  final int? height;
+
+  const AppstreamCachedIcon(this.name, {this.width, this.height});
+
+  @override
+  String toString() => '$runtimeType($name)';
+}
+
+class AppstreamLocalIcon extends AppstreamIcon {
+  final String filename;
+  final int? width;
+  final int? height;
+
+  const AppstreamLocalIcon(this.filename, {this.width, this.height});
+
+  @override
+  String toString() => '$runtimeType($filename)';
+}
+
+class AppstreamRemoteIcon extends AppstreamIcon {
+  final String url;
+  final int? width;
+  final int? height;
+
+  const AppstreamRemoteIcon(this.url, {this.width, this.height});
+
+  @override
+  String toString() => '$runtimeType($url)';
+}
+
 class AppstreamComponent {
   final String id;
   final String type;
   final String package;
   final Map<String, String> name;
   final Map<String, String> summary;
+  final List<AppstreamIcon> icons;
 
   const AppstreamComponent(
       {required this.id,
       required this.type,
       required this.package,
       required this.name,
-      required this.summary});
+      required this.summary,
+      this.icons = const []});
 
   @override
   String toString() =>
-      '$runtimeType(id: $id, type: $type, package: $package, name: $name, summary: $summary)';
+      '$runtimeType(id: $id, type: $type, package: $package, name: $name, summary: $summary, icons: $icons)';
 }
 
 class AppstreamCollection {
   final String version;
   final String origin;
-  final String? mediaBaseUrl;
   final String? architecture;
   final int? priority;
   final List<AppstreamComponent> components;
@@ -31,7 +78,6 @@ class AppstreamCollection {
   AppstreamCollection(
       {this.version = '0.14',
       required this.origin,
-      this.mediaBaseUrl,
       this.architecture,
       this.priority,
       Iterable<AppstreamComponent> components = const []})
@@ -73,12 +119,45 @@ class AppstreamCollection {
       }
       var name = _getXmlTranslatedString(component, 'name');
       var summary = _getXmlTranslatedString(component, 'summary');
+
+      var icons = <AppstreamIcon>[];
+      for (var icon in component.children
+          .whereType<XmlElement>()
+          .where((e) => e.name.local == 'icon')) {
+        var type = icon.getAttribute('type');
+        if (type == null) {
+          throw FormatException('Missing icon type');
+        }
+        var w = icon.getAttribute('width');
+        var width = w != null ? int.parse(w) : null;
+        var h = icon.getAttribute('height');
+        var height = h != null ? int.parse(h) : null;
+        switch (type) {
+          case 'stock':
+            icons.add(AppstreamStockIcon(icon.text));
+            break;
+          case 'cached':
+            icons.add(
+                AppstreamCachedIcon(icon.text, width: width, height: height));
+            break;
+          case 'local':
+            icons.add(
+                AppstreamLocalIcon(icon.text, width: width, height: height));
+            break;
+          case 'remote':
+            icons.add(
+                AppstreamRemoteIcon(icon.text, width: width, height: height));
+            break;
+        }
+      }
+
       components.add(AppstreamComponent(
           id: id.text,
           type: type,
           package: package.text,
           name: name,
-          summary: summary));
+          summary: summary,
+          icons: icons));
     }
 
     return AppstreamCollection(
@@ -134,18 +213,49 @@ class AppstreamCollection {
       if (summary == null) {
         throw FormatException('Missing component summary');
       }
+
+      var icons = <AppstreamIcon>[];
+      var icon = component['Icon'];
+      if (icon != null) {
+        for (var type in icon.keys) {
+          switch (type) {
+            case 'stock':
+              icons.add(AppstreamStockIcon(icon[type]));
+              break;
+            case 'cached':
+              for (var i in icon[type]) {
+                icons.add(AppstreamCachedIcon(i['name'],
+                    width: i['width'], height: i['height']));
+              }
+              break;
+            case 'local':
+              for (var i in icon[type]) {
+                icons.add(AppstreamLocalIcon(i['name'],
+                    width: i['width'], height: i['height']));
+              }
+              break;
+            case 'remote':
+              for (var i in icon[type]) {
+                icons.add(AppstreamRemoteIcon(_makeUrl(mediaBaseUrl, i['url']),
+                    width: i['width'], height: i['height']));
+              }
+              break;
+          }
+        }
+      }
+
       components.add(AppstreamComponent(
           id: id,
           type: type,
           package: package,
           name: _parseYamlTranslatedString(name),
-          summary: _parseYamlTranslatedString(summary)));
+          summary: _parseYamlTranslatedString(summary),
+          icons: icons));
     }
 
     return AppstreamCollection(
         version: version,
         origin: origin,
-        mediaBaseUrl: mediaBaseUrl,
         architecture: architecture,
         priority: priority,
         components: components);
@@ -173,4 +283,16 @@ Map<String, String> _getXmlTranslatedString(XmlElement parent, String name) {
   }
 
   return value;
+}
+
+String _makeUrl(String? mediaBaseUrl, String url) {
+  if (mediaBaseUrl == null) {
+    return url;
+  }
+
+  if (url.startsWith('http:')) {
+    return url;
+  }
+
+  return mediaBaseUrl + '/' + url;
 }
